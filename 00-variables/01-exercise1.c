8a111e1ad60e241c27c3088b906f81e1727ee577
h.annane <hamza.annane.office@gmail.com>
2025-08-03 11:19:57 +0100

    ✅ Exercise 1: Define and Print Variables

---
+++ 00-variables/01-exercise1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int     main()
+{
+    int a = 3;
+    int b = 5;
+    int sum = a + b;
+    char sentence[] = "the sum is: ";
+
+    printf("%s%d\n",sentence,sum);
+
+    return 0;
+}
